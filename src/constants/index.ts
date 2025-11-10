e2fce2881ad7f81a4b83b59a23ef5008df17e85d
Usman Suleiman Babakolo <usmansbk@gmail.com>
2025-11-10 12:53:23 +0100

    Update API configuration to set APP_ENV to a hardcoded value for development

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,7 +4,7 @@ const baseUrl = "http://192.168.0.166:8082";
 export const API_CONFIG = {
   GRAPHQL_URL: `${baseUrl}/graphql`,
   REST_API_URL: `${baseUrl}/v1`,
-  APP_ENV: process.env.EXPO_PUBLIC_APP_ENV || "development",
+  APP_ENV: "development",
   CLIENT_ID: "cmetwfh3v00018oj12woe6gpc",
 };
 
