112aa58f3665d8eb3d8dc284235e55e81e84d2fb
Abdullah Gumi <abdulgumi77@gmail.com>
2025-11-13 07:27:56 +0100

    refactor: use cache-first policy for GET_USER_SESSION

--- src/hooks/useCurrentUser/index.ts
+++ src/hooks/useCurrentUser/index.ts
@@ -2,7 +2,9 @@ import { useQuery } from "@apollo/client/react";
 import { GET_USER_SESSION } from "../useLoginWithPassword/mutation";
 
 export default function useCurrentUser() {
-  const { data, loading, error } = useQuery(GET_USER_SESSION);
+  const { data, loading, error } = useQuery(GET_USER_SESSION, {
+    fetchPolicy: "cache-first",
+  });
 
   return {
     user: data?.me,
